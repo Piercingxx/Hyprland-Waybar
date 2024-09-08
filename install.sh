586f3dcd8aad7fd9dfb44cf4e6eb7717fac8d8dd
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2024-09-08 11:09:33 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+  
+
+# Update system and install Git
+
+sudo pacman -Syu git --noconfirm
+
+  
+
+# Clone and install Paru
+
+git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si --noconfirm && cd ..
+
+  
+
+# Install necessary packages using Paru
+
+paru -Syu cliphist hyprland waybar alacritty rofi-wayland swww libnotify notification-daemon swaync networkmanager network-manager-applet bluez bluez-utils blueman papirus-icon-theme noto-fonts-emoji fonts-firacode fonts-font-awesome fonts-noto fonts-noto-cjk fonts-noto-color-emoji lxsession brightnessctl light nwg-look yad sox mint-themes xcursor-simp1e-gruvbox-light hyprlock hypridle hyprpicker qt5-wayland eww xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland --noconfirm
+
+
+mkdir -p $HOME/.fonts
+cd $HOME/.fonts
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v${nerd_font_version}/${nerd_font}.zip"
+unzip "${nerd_font}.zip"
+rm OFL.txt
+rm readme.md
+rm "${nerd_font}.zip"
