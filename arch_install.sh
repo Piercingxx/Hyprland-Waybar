d64ef06fe4dd5cca81f706e3118dd1d141c69ac6
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2024-09-26 13:23:04 -0400

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -21,34 +21,32 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm && cd ..
 
+
+# First round of installs
+yay -S hyprshot
+yay -S eww
+yay -S rofi
+
+
 # Install necessary packages using Paru
 paru -Syu 
 paru -S cliphist --noconfirm
 paru -S hyprland --noconfirm
 paru -S hyprpaper --noconfirm
-paru -S hyprshot --noconfirm
 paru -S hyprlock --noconfirm
 paru -S hypridle --noconfirm
-paru -S hyprpicke --noconfirm
+paru -S hyprpicker --noconfirm
 paru -S waybar --noconfirm
 paru -S wl-gammarelay --noconfirm
-paru -S rofi-wayl --noconfirm
 paru -S swww --noconfirm
 paru -S libnotify --noconfirm
-paru -S notificat --noconfirm
+paru -S notification-daemon --noconfirm
 paru -S swaync --noconfirm
-paru -S networkma --noconfirm
-paru -S network-m --noconfirm
+paru -S networkmanager --noconfirm
+paru -S network-manager-applet --noconfirm
 paru -S bluez --noconfirm
 paru -S bluez-uti --noconfirm
 paru -S blueman --noconfirm
-paru -S papirus-i --noconfirm
-paru -S noto-font --noconfirm
-paru -S fonts-fir --noconfirm
-paru -S fonts-fon --noconfirm
-paru -S fonts-not --noconfirm
-paru -S fonts-not --noconfirm
-paru -S fonts-not --noconfirm
 paru -S lxsession --noconfirm
 paru -S brightnessctl --noconfirm
 paru -S light --noconfirm
@@ -58,16 +56,22 @@ paru -S sox --noconfirm
 paru -S mint-themes --noconfirm
 paru -S xcursor-simp1e-gruvbox-light --noconfirm
 paru -S qt5-wayland --noconfirm
-paru -S eww --noconfirm
 paru -S xdg-desktop-portal --noconfirm
 paru -S xdg-desktop-portal-gtk --noconfirm
 paru -S xdg-desktop-portal-hyprland --noconfirm
+paru -S papirus-icon-theme --noconfirm
+paru -S ttf-firacode --noconfirm
+paru -S awesome-terminal-fonts --noconfirm
+paru -S ttf-ms-fonts --noconfirm
+paru -S terminus-font-ttf --noconfirm
+paru -S noto-color-emoji-fontconfig-no-binding --noconfirm
 
 
 mkdir -p $HOME/.fonts
 cd $HOME/.fonts
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v${nerd_font_version}/${nerd_font}.zip"
-unzip "${nerd_font}.zip"
-rm OFL.txt
-rm readme.md
-rm "${nerd_font}.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip
+rm Firacode.zip
+rm Meslo.zip
