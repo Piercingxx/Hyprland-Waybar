94627c75c273de80b5b616b16a11f0fbb659455a
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-03-13 12:27:22 -0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-sudo pacman -Syu
-sudo pacman -S zip unzip gzip tar make --noconfirm 
+#!/bin/bash
 
-
-# Enable Bluetooth
-sudo systemctl start bluetooth
-systemctl enable bluetooth
-
-# Enable Printer 
-sudo pacman -S cups gutenprint cups-pdf gtk3-print-backends nmap net-tools cmake meson cpio -y
-systemctl enable cups.service
-systemctl start cups
-
-
-# Update system and install Git
-sudo pacman -Syu git --noconfirm
+# Check if running as root. If root, script will exit
+if [[ $EUID -eq 0 ]]; then
+    echo "This script should not be executed as root! Exiting......."
+    exit 1
+fi
 
 # Clone and install Paru
 git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si --noconfirm && cd ..
@@ -24,10 +15,21 @@ git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si -
 sudo pacman -S flatpak --noconfirm
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+sudo pacman -Syu
+sudo pacman -S zip unzip gzip tar make --noconfirm
 
+# Enable Bluetooth
+sudo systemctl start bluetooth
+systemctl enable bluetooth
+
+# Enable Printer 
+sudo pacman -S cups gutenprint cups-pdf gtk3-print-backends nmap net-tools cmake meson cpio --noconfirm
+systemctl enable cups.service
+systemctl start cups
 
 #Synology Drive doesnt support wayland so run this..
 QT_QPA_PLATFORM=xcb
 #Steam audio issues so run this..
 ALSOFT_DRIVERS=pulse
 
+sudo reboot
